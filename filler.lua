2cb57b37f66a73260bbc4f5d036773715a645437
Quote Pilgrim <caio.psv@gmail.com>
2026-05-07 01:03:32 -0300

    updated filler text generator

--- filler.lua
+++ filler.lua
@@ -1,20 +1,109 @@
-local alphabet = "abcdefghijklmnopqrstuvwxyz"
+local random = love.math.random
+
+local tokens = {
+	V = { "a", "e", "i", "o", "u" },
+	C = { "p", "t", "k", "f", "s", "b", "d", "g", "v", "z", "n" },
+	C2 = { "p", "t", "k", "b", "d", "g" },
+	N = { "s", "n" },
+	S = { "y", "s", "h" },
+	--
+	B = { "V", "M" },
+	M = { "C V:3", "C V N:2", "M2" },
+	M2 = { "C2 S V", "C2 S V N" },
+	E = { "M", "M C" },
+	--
+	W = { "E", "B M:2", "B M E:2", "V C" },
+}
+
+local replace = {
+	{ "nn", "n" },
+	{ "ss", "sh" },
+	{ "sz", "z" },
+}
+
+for _, t in pairs(tokens) do
+	for i = #t, 1, -1 do
+		local token, freq = t[i]:match("(.+):(.+)")
+
+		if freq then
+			table.remove(t, i)
+			for _ = 1, tonumber(freq) do
+				table.insert(t, token)
+			end
+		end
+	end
+end
+
+local function split(s)
+	local result = {}
+
+	for match in s:gmatch("[^%s]+") do
+		table.insert(result, match)
+	end
+
+	return result
+end
 
 local function word_gen()
-	local word = {}
+	local num = random(#tokens.W)
+	local word = split(tokens.W[num])
 
-	while true do
-		local i = love.math.random(#alphabet)
-		table.insert(word, alphabet:sub(i, i))
+	local replaced = true
 
-		if love.math.random() < 0.2 then
-			break
+	while replaced do
+		replaced = false
+
+		for i, v in ipairs(word) do
+			if tokens[v] then
+				replaced = true
+				table.remove(word, i)
+
+				local t = split(tokens[v][random(#tokens[v])])
+
+				for j = #t, 1, -1 do
+					table.insert(word, i, t[j])
+				end
+
+				break
+			end
 		end
 	end
 
-	return table.concat(word)
+	local result = table.concat(word)
+
+	for _, rule in ipairs(replace) do
+		result = result:gsub(unpack(rule))
+	end
+
+	return result
 end
 
-for _ = 1, 10 do
-	print(word_gen())
+local function sentence_gen(min, max)
+	min = min or 3
+	max = max or 20
+
+	local count = random(min, max)
+	local sentence = {}
+
+	for i = 1, count do
+		if random() < 0.1 and i < count then
+			table.insert(sentence, word_gen() .. ",")
+		else
+			table.insert(sentence, word_gen())
+		end
+	end
+
+	sentence[1] = sentence[1]:sub(1, 1):upper() .. sentence[1]:sub(2)
+
+	return table.concat(sentence, " ") .. "."
 end
+
+local text = {}
+for _ = 1, 10 do
+	table.insert(text, sentence_gen())
+end
+
+local result = table.concat(text, " ")
+
+love.system.setClipboardText(result)
+print(result)
--- poker.lua
+++ poker.lua
@@ -1,60 +1,13 @@
 local shuffle = require("shuffle")
 local combinations = require("combinations")
 
-local cards = {
-	{ 2, "S" },
-	{ 3, "S" },
-	{ 4, "S" },
-	{ 5, "S" },
-	{ 6, "S" },
-	{ 7, "S" },
-	{ 8, "S" },
-	{ 9, "S" },
-	{ 10, "S" },
-	{ 11, "S" },
-	{ 12, "S" },
-	{ 13, "S" },
-	{ 14, "S" },
-	{ 2, "H" },
-	{ 3, "H" },
-	{ 4, "H" },
-	{ 5, "H" },
-	{ 6, "H" },
-	{ 7, "H" },
-	{ 8, "H" },
-	{ 9, "H" },
-	{ 10, "H" },
-	{ 11, "H" },
-	{ 12, "H" },
-	{ 13, "H" },
-	{ 14, "H" },
-	{ 2, "C" },
-	{ 3, "C" },
-	{ 4, "C" },
-	{ 5, "C" },
-	{ 6, "C" },
-	{ 7, "C" },
-	{ 8, "C" },
-	{ 9, "C" },
-	{ 10, "C" },
-	{ 11, "C" },
-	{ 12, "C" },
-	{ 13, "C" },
-	{ 14, "C" },
-	{ 2, "D" },
-	{ 3, "D" },
-	{ 4, "D" },
-	{ 5, "D" },
-	{ 6, "D" },
-	{ 7, "D" },
-	{ 8, "D" },
-	{ 9, "D" },
-	{ 10, "D" },
-	{ 11, "D" },
-	{ 12, "D" },
-	{ 13, "D" },
-	{ 14, "D" },
-}
+local cards = {}
+
+for _, s in ipairs({ "S", "H", "C", "D" }) do
+	for r = 2, 14 do
+		table.insert(cards, { r, s })
+	end
+end
 
 local hand_types = {
 	highcard = 0,
@@ -162,10 +115,8 @@ local function is_better(a, b)
 	local a_type = get_type(a)
 	local b_type = get_type(b)
 
-	if a_type > b_type then
-		return true
-	elseif b_type > a_type then
-		return false
+	if a_type ~= b_type then
+		return a_type > b_type
 	end
 
 	for i = 1, 5 do
