5dad5281ba59a02b4bae0a53240aa046b0dd674f
quotepilgrim <caio.psv@gmail.com>
2026-06-21 22:08:20 -0300

    added polypoly

--- tests.lua
+++ tests.lua
@@ -1,5 +1,6 @@
 local backwards = require("backwards")
 local split_groups = require("splitgroups")
+local polypoly = require("polypoly")
 
 local str = "the quick brown fox jumps over the lazy dog"
 local fmt = "%-16s %s"
@@ -8,3 +9,10 @@ print(fmt:format("base string:", str))
 print(fmt:format("backwards:", backwards(str)))
 print(fmt:format("groups of 4:", split_groups(str, 4)))
 print(fmt:format("groups of 5:", split_groups(str)))
+
+local ciphertext = polypoly.encode(split_groups(str))
+
+print()
+print(ciphertext)
+print(polypoly.decode(ciphertext, true))
+print(polypoly.decode(ciphertext))
